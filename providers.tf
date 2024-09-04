8217bd7dcb3f3f64ad4733c6f19b50c4fbde1530
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-09-04 16:25:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.109.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "demorg"
+      storage_account_name = "terraformstate004"
+      container_name       = "tfstatefiles"
+      key                  = "terraform.tfstate"
+      access_key           = "gt4Bfhg8Om89VdWhI/Yv3N+zWV2M/PR3aNS08DloI2oRwec1L3rF1WTd3To7MnS5s49Rl9vpG20S+AStL66FUw=="
+  }
 }
 
 provider "azurerm" {
