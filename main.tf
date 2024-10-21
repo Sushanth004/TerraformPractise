bbac532e063fbc07227a2df1e0b3bc1ffa23c377
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-21 14:28:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,13 +25,13 @@ resource "azurerm_cosmosdb_mongo_database" "example" {
   account_name        = resource.azurerm_cosmosdb_account.example.name
 }
 
-resource "azurerm_cosmosdb_mongo_collection" "example" {
-  name                = "cosmos-mongo-db-collection"
-  resource_group_name = "demorg"
-  account_name        = resource.azurerm_cosmosdb_account.example.name
-  database_name       = resource.azurerm_cosmosdb_mongo_database.example.name
+#resource "azurerm_cosmosdb_mongo_collection" "example" {
+#  name                = "cosmos-mongo-db-collection"
+#  resource_group_name = "demorg"
+#  account_name        = resource.azurerm_cosmosdb_account.example.name
+#  database_name       = resource.azurerm_cosmosdb_mongo_database.example.name
 
-  default_ttl_seconds = "777"
-  shard_key           = "uniqueKey"
-  throughput          = 400
-}
+#  default_ttl_seconds = "777"
+#  shard_key           = "uniqueKey"
+#  throughput          = 400
+#}
