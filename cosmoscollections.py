bca308b2f8ba5f249ba6984f848d3ccd96b8ebc9
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-22 12:03:28 +0530

    Update cosmoscollections.py

--- cosmoscollections.py
+++ cosmoscollections.py
@@ -3,21 +3,21 @@ stratosphere_cosmos_collections = [
     "account_name" : "cosmosacc0044",
     "database_name" : "cosmos-mongo-db",
     "collection_name" : "collectionA",
-    "max_ru" : 1000,
+    "max_ru" : "1000",
     "shard_key" : "tenantA"
   },
   {
     "account_name" : "cosmosacc0044",
     "database_name" : "cosmos-mongo-db",
     "collection_name" : "collectionB",
-    "max_ru" : 1000,
+    "max_ru" : "1000",
     "shard_key" : "tenantA"
   },
   {
     "account_name" : "cosmosacc0044",
     "database_name" : "cosmos-mongo-db",
     "collection_name" : "collectionC",
-    "max_ru" : 1000,
+    "max_ru" : "1000",
     "shard_key" : "tenantB"
   }
 ]
