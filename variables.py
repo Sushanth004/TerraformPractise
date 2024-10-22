89c479a0ad7faf66d16eb48ef756ce20c0a0c926
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-22 09:45:31 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,27 @@
+stratosphere_cosmos_collections = [
+  {
+    name      = "collectionA"
+    ru        = 1000
+    shard_key = "tenantA"
+  },
+  {
+    name      = "collectionB"
+    ru        = 1000
+    shard_key = "tenantA"
+  },
+  {
+    name      = "collectionC"
+    ru        = 20000
+    shard_key = "tenantB"
+  },
+  {
+    name      = "collectionD"
+    ru        = 20000
+    shard_key = "tenantB"
+  },
+  {
+    name      = "collectionE"
+    ru        = 1000
+    shard_key = "tenantA"
+  }
+]
