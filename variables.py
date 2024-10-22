4686294856b9107ffa322cd509ccd68453fc52ee
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-22 10:03:22 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,35 +1,35 @@
 stratosphere_cosmos_collections = [
   {
-    account_name = ""
-    database_name = ""
+    account_name = "cosmosacc0044"
+    database_name = "cosmos-mongo-db"
     collection_name      = "collectionA"
     max_ru        = 1000
     shard_key = "tenantA"
   },
   {
-    account_name = ""
-    database_name = ""
+    account_name = "cosmosacc0044"
+    database_name = "cosmos-mongo-db"
     collection_name = "collectionB"
     ru        = 1000
     shard_key = "tenantA"
   },
   {
-    account_name = ""
-    database_name = ""
+    account_name = "cosmosacc0044"
+    database_name = "cosmos-mongo-db"
     collection_name = "collectionC"
     ru        = 20000
     shard_key = "tenantB"
   },
   {
-    account_name = ""
-    database_name = ""
+    account_name = "cosmosacc0044"
+    database_name = "cosmos-mongo-db"
     collection_name = "collectionD"
     ru        = 20000
     shard_key = "tenantB"
   },
   {
-    account_name = ""
-    database_name = ""
+    account_name = "cosmosacc0044"
+    database_name = "cosmos-mongo-db"
     collection_name = "collectionE"
     ru        = 1000
     shard_key = "tenantA"
