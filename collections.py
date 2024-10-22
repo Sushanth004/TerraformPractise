27a441bd3764b224665122ff4745a7f7ccb71d4f
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-22 10:24:52 +0530

    Update collections.py

--- collections.py
+++ collections.py
@@ -1,37 +1,37 @@
 stratosphere_cosmos_collections = [
   {
-    "account_name" : "cosmosacc0044"
-    "database_name" : "cosmos-mongo-db"
-    "collection_name"      : "collectionA"
-    "max_ru"        : 1000
+    "account_name" : "cosmosacc0044",
+    "database_name" : "cosmos-mongo-db",
+    "collection_name" : "collectionA",
+    "max_ru" : 1000,
     "shard_key" : "tenantA"
   },
   {
-    "account_name" : "cosmosacc0044"
-    "database_name" : "cosmos-mongo-db"
-    "collection_name" : "collectionB"
-    "max_ru"        : 1000
+    "account_name" : "cosmosacc0044",
+    "database_name" : "cosmos-mongo-db",
+    "collection_name" : "collectionB",
+    "max_ru" : 1000,
     "shard_key" : "tenantA"
   },
   {
-    "account_name" : "cosmosacc0044"
-    "database_name" : "cosmos-mongo-db"
-    "collection_name" : "collectionC"
-    "max_ru"        : 20000
+    "account_name" : "cosmosacc0044",
+    "database_name" : "cosmos-mongo-db",
+    "collection_name" : "collectionC",
+    "max_ru" : 20000,
     "shard_key" : "tenantB"
   },
   {
-    "account_name" : "cosmosacc0044"
-    "database_name" : "cosmos-mongo-db"
-    "collection_name" : "collectionD"
-    "max_ru"        : 20000
+    "account_name" : "cosmosacc0044",
+    "database_name" : "cosmos-mongo-db",
+    "collection_name" : "collectionD",
+    "max_ru" : 20000,
     "shard_key" : "tenantB"
   },
   {
-    "account_name" : "cosmosacc0044"
-    "database_name" : "cosmos-mongo-db"
-    "collection_name" : "collectionE"
-    "max_ru"        : 1000
+    "account_name" : "cosmosacc0044",
+    "database_name" : "cosmos-mongo-db",
+    "collection_name" : "collectionE",
+    "max_ru" : 1000,
     "shard_key" : "tenantA"
   }
 ]
