8c0acd4d6381b4eb49c51aa83fdc0fb5b95ed411
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-22 12:35:25 +0530

    Delete cosmoscollections.sh

--- cosmoscollections.sh
+++
@@ -1,1 +0,0 @@
-az cosmosdb mongodb collection create -g demorg -a "cosmosacc0044" -d "cosmos-mongo-db" -n "collectionB" --shard "tenantA" --idx "[{\"key\": {\"keys\": [\"\$**\"]}}]" --max-throughput "4000"
