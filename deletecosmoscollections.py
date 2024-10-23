095216b6e4acffdaf85060dd63e9e2442d5961c2
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-24 00:29:48 +0530

    Create deletecosmoscollections.py

---
+++ deletecosmoscollections.py
@@ -0,0 +1,9 @@
+stratosphere_cosmos_collections = [
+  {
+    "account_name" : "cosmosacc0044",
+    "database_name" : "cosmos-mongo-db",
+    "collection_name" : "collectionE",
+    "max_ru" : "1000",
+    "shard_key" : "tenantB"
+  }
+]
