616f3042b8bbac1783e87b4496e9b7e75c5d2400
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-28 18:42:47 +0530

    Update cosmoscollections.py

--- cosmoscollections.py
+++ cosmoscollections.py
@@ -38,12 +38,13 @@ for stratosphere_cosmos_collection in stratosphere_cosmos_collections:
        command="az cosmosdb mongodb collection throughput show --account-name "+stratosphere_cosmos_collection["account_name"]+" --database-name "+stratosphere_cosmos_collection["database_name"]+" --name "+ stratosphere_cosmos_collection["collection_name"] +" --resource-group demorg"
        collection_throughtput_details_azure = subprocess.run(command, shell=True, capture_output=True, text=True)
        collections_details_azure = json.loads(collection_throughtput_details_azure.stdout)
-       if stratosphere_cosmos_collection["max_ru"] != collections_details_azure["resource"]["autoscaleSettings"]["maxThroughput"]:
-          print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}")
-          command="az cosmosdb mongodb collection throughput update -g demorg -a "+stratosphere_cosmos_collection['account_name']+" -d "+stratosphere_cosmos_collection['database_name']+" -n "+stratosphere_cosmos_collection['collection_name']+" --max-throughput "+stratosphere_cosmos_collection['max_ru']
-          result = subprocess.run(command, shell=True, capture_output=True, text=True)
-          print(result.stdout)
-          print(result.stderr)
+       print(collections_details_azure["resource"]["autoscaleSettings"]["maxThroughput"])
+       # if stratosphere_cosmos_collection["max_ru"] != collections_details_azure["resource"]["autoscaleSettings"]["maxThroughput"]:
+       #    print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}")
+       #    command="az cosmosdb mongodb collection throughput update -g demorg -a "+stratosphere_cosmos_collection['account_name']+" -d "+stratosphere_cosmos_collection['database_name']+" -n "+stratosphere_cosmos_collection['collection_name']+" --max-throughput "+stratosphere_cosmos_collection['max_ru']
+       #    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+       #    print(result.stdout)
+       #    print(result.stderr)
   
 #env_file.write('az cosmosdb mongodb collection create -g demorg -a '+stratosphere_cosmos_collection['account_name']+' -d '+stratosphere_cosmos_collection['database_name']+' -n '+stratosphere_cosmos_collection['collection_name']+' --shard '+stratosphere_cosmos_collection['shard_key']+' --idx \'[{\"key\": {\"keys\": [\"$**\"]}}]\' --max-throughput '+stratosphere_cosmos_collection['max_ru']+'\n')
   
