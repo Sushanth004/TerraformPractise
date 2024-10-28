40a3e30592cdf2293129cd76299024cc1cdfc693
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-28 23:48:53 +0530

    Update cosmoscollections.py

--- cosmoscollections.py
+++ cosmoscollections.py
@@ -48,9 +48,8 @@ existing_cosmos_collections = []
 for collection in collections_details:
   existing_cosmos_collections.append(collection["name"])
 
-print(f"Existing Cosmos Collection {existing_cosmos_collections}")
-
 def createCollections(stratosphere_cosmos_collection):
+  print(f"Creating cosmos collection {stratosphere_cosmos_collection['collection_name']}")
   command="az cosmosdb mongodb collection create -g demorg -a "+stratosphere_cosmos_collection['account_name']+" -d "+stratosphere_cosmos_collection['database_name']+" -n "+stratosphere_cosmos_collection['collection_name']+" --shard "+stratosphere_cosmos_collection['shard_key']+" --idx \'[{\"key\": {\"keys\": [\"$**\"]}}]\' --max-throughput "+stratosphere_cosmos_collection['max_ru']
   result = subprocess.run(command, shell=True, capture_output=True, text=True)
   print(result.stdout)
@@ -61,7 +60,7 @@ def updateCollections(stratosphere_cosmos_collection):
   collection_throughtput_details_azure = subprocess.run(command, shell=True, capture_output=True, text=True)
   collections_details_azure = json.loads(collection_throughtput_details_azure.stdout)
   if stratosphere_cosmos_collection["max_ru"] != str(collections_details_azure["resource"]["autoscaleSettings"]["maxThroughput"]):
-    print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}")
+    print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}, updating the collection")
     command="az cosmosdb mongodb collection throughput update -g demorg -a "+stratosphere_cosmos_collection['account_name']+" -d "+stratosphere_cosmos_collection['database_name']+" -n "+stratosphere_cosmos_collection['collection_name']+" --max-throughput "+stratosphere_cosmos_collection['max_ru']
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     print(result.stdout)
