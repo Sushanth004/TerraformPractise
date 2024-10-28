e3858503ef74e8ab5f251e5c8fc852f145c4c40b
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-28 18:34:35 +0530

    Update cosmoscollections.py

--- cosmoscollections.py
+++ cosmoscollections.py
@@ -39,7 +39,7 @@ for stratosphere_cosmos_collection in stratosphere_cosmos_collections:
        collection_throughtput_details_azure = subprocess.run(command, shell=True, capture_output=True, text=True)
        collections_details_azure = json.loads(collection_throughtput_details_azure.stdout)
        if stratosphere_cosmos_collection["max_ru"] != collections_details_azure["resource"]["autoscaleSettings"]["maxThroughput"]:
-          Print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}")
+          print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}")
           command="az cosmosdb mongodb collection throughput update -g demorg -a "+stratosphere_cosmos_collection['account_name']+" -d "+stratosphere_cosmos_collection['database_name']+" -n "+stratosphere_cosmos_collection['collection_name']+" --max-throughput "+stratosphere_cosmos_collection['max_ru']
           result = subprocess.run(command, shell=True, capture_output=True, text=True)
           print(result.stdout)
