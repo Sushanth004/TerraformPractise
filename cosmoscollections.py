45dfad1e3b31f2209ac6c1fc8873f11fcaca837f
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-28 18:23:08 +0530

    Update cosmoscollections.py

--- cosmoscollections.py
+++ cosmoscollections.py
@@ -3,7 +3,7 @@ stratosphere_cosmos_collections = [
     "account_name" : "cosmosacc0044",
     "database_name" : "cosmos-mongo-db",
     "collection_name" : "collectionA",
-    "max_ru" : "3000",
+    "max_ru" : "2000",
     "shard_key" : "tenantA"
   },
   {
@@ -34,10 +34,12 @@ collections_details = json.loads(result.stdout)
 for stratosphere_cosmos_collection in stratosphere_cosmos_collections:
   for collection_details in collections_details:
     if stratosphere_cosmos_collection["collection_name"] == collection_details["name"]:
+       print(f"collection details matched {stratosphere_cosmos_collection['collection_name']}")
        command="az cosmosdb mongodb collection throughput show --account-name "+stratosphere_cosmos_collection["account_name"]+" --database-name "+stratosphere_cosmos_collection["database_name"]+" --name "+ stratosphere_cosmos_collection["collection_name"] +" --resource-group demorg"
        collection_throughtput_details_azure = subprocess.run(command, shell=True, capture_output=True, text=True)
        collections_details_azure = json.loads(collection_throughtput_details_azure.stdout)
        if stratosphere_cosmos_collection["max_ru"] != collections_details_azure["resource"]["autoscaleSettings"]["maxThroughput"]:
+          Print(f"Max RUs is different for collection {stratosphere_cosmos_collection['collection_name']}")
           command="az cosmosdb mongodb collection throughput update -g demorg -a "+stratosphere_cosmos_collection['account_name']+" -d "+stratosphere_cosmos_collection['database_name']+" -n "+stratosphere_cosmos_collection['collection_name']+" --max-throughput "+stratosphere_cosmos_collection['max_ru']
           result = subprocess.run(command, shell=True, capture_output=True, text=True)
           print(result.stdout)
