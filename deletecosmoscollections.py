101dcf3b96eafbbccdd8c8c710178f859e97b748
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-28 12:32:31 +0530

    Update deletecosmoscollections.py

--- deletecosmoscollections.py
+++ deletecosmoscollections.py
@@ -1,12 +1,1 @@
-stratosphere_cosmos_collections = [
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionE"
-  },
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionZ"
-  }
-]
+stratosphere_cosmos_collections = []
