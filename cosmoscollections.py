89466bcbde47488378aafa0511127d98b2851fe6
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-10-30 11:05:38 +0530

    Update cosmoscollections.py

--- cosmoscollections.py
+++ cosmoscollections.py
@@ -1,40 +1,4 @@
-stratosphere_cosmos_collections = [
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionA",
-    "max_ru" : "2000",
-    "shard_key" : "tenantA"
-  },
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionB",
-    "max_ru" : "1000",
-    "shard_key" : "tenantA"
-  },
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionC",
-    "max_ru" : "1000",
-    "shard_key" : "tenantB"
-  },
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionD",
-    "max_ru" : "1000",
-    "shard_key" : "tenantB"
-  },
-  {
-    "account_name" : "cosmosacc0044",
-    "database_name" : "cosmos-mongo-db",
-    "collection_name" : "collectionE",
-    "max_ru" : "1000",
-    "shard_key" : "tenantB"
-  }
-]
+stratosphere_cosmos_collections = 
 
 
 import os 
