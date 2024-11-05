ed4782cfd81197d7a296b145990f1ca78e1e0a86
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-11-05 22:18:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-resource "azurerm_cosmosdb_account" "example" {
-  name                  = "cosmosacc0044"
-  location              = "East US"
-  resource_group_name   = "demorg"
-  offer_type            = "Standard"
-  kind                  = "MongoDB"
+#resource "azurerm_cosmosdb_account" "example" {
+#  name                  = "cosmosacc0044"
+#  location              = "East US"
+#  resource_group_name   = "demorg"
+#  offer_type            = "Standard"
+#  kind                  = "MongoDB"
 
-  capabilities {
-    name = "EnableMongo"
-  }
+#  capabilities {
+#    name = "EnableMongo"
+#  }
 
-  consistency_policy {
-    consistency_level = "Strong"
-  }
+#  consistency_policy {
+#    consistency_level = "Strong"
+#  }
 
-  geo_location {
-    location          = "East US"
-    failover_priority = 0
-  }
-}
+#  geo_location {
+#    location          = "East US"
+#    failover_priority = 0
+#  }
+#}
 
-resource "azurerm_cosmosdb_mongo_database" "example" {
-  name                = "cosmos-mongo-db"
-  resource_group_name = "demorg"
-  account_name        = resource.azurerm_cosmosdb_account.example.name
-}
+#resource "azurerm_cosmosdb_mongo_database" "example" {
+#  name                = "cosmos-mongo-db"
+#  resource_group_name = "demorg"
+#  account_name        = resource.azurerm_cosmosdb_account.example.name
+#}
 
 #resource "azurerm_cosmosdb_mongo_collection" "example" {
 #  name                = "cosmos-mongo-db-collection"
