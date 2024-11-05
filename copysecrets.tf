897266edd61b299953b5bfc089063a401372a1ae
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-11-05 22:18:56 +0530

    Create copysecrets.tf

---
+++ copysecrets.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "default" {
+  name     = "demorg"
+  location = "East US 2"
+}
+
+resource "azurerm_eventgrid_topic" "test" {
+  name                = "my-eventgrid-topic"
+  location            = "East US 2"
+  resource_group_name = "demorg"
+}
+
+// resource "azurerm_eventgrid_event_subscription" "default" {
+//   name  = "defaultEventSubscription"
+//   scope = "${azurerm_resource_group.default.id}"
+//   event_delivery_schema = "EventGridSchema"
+//   topic_name = "my-eventgrid-topic"
+
+//   webhook_endpoint {
+//     url = "https://my-webhook-endpoint.example.com/notify"
+//   }
+// }
