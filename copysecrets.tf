87005c191d186084e8566faa9c01a5168c525fdb
Sushanth004 <35901127+Sushanth004@users.noreply.github.com>
2024-11-05 22:25:52 +0530

    Update copysecrets.tf

--- copysecrets.tf
+++ copysecrets.tf
@@ -1,3 +1,25 @@
+resource "azurerm_key_vault" "keyvaultA" {
+  name                        = "keyvault0044A"
+  location                    = "East US 2"
+  resource_group_name         = "demorg"
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault" "keyvaultB" {
+  name                        = "keyvault0044B"
+  location                    = "East US 2"
+  resource_group_name         = "demorg"
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
 resource "azurerm_eventgrid_topic" "test" {
   name                = "my-eventgrid-topic"
   location            = "East US 2"
